554e6db488f32e7c3c9c65fd9906cbdeeaca2a89
Shareiar <islamshareiar@gmail.com>
2025-05-19 04:00:30 +0600

    create primary layout for whole project and add nav and top content in this layout for using all pages

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <!-- Top Bar -->
+    <TopBar />
+    <!-- Nav Bar -->
+    <NavBar />
+    <!-- main content part -->
+    <main class="main-content">
+        <router-view />
+    </main>
+    <!-- footer -->
+    <footer>
+        
+    </footer>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+import TopBar from '@/components/TopBar.vue';
+
+
+export default {
+    name: 'MainLayout',
+    components: {
+        NavBar, TopBar
+    }
+}
+</script>
+
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .main-content{
+        padding: 0;
+        margin: 0;
+        width: 100%;
+    }
+</style>
