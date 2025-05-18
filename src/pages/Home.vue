1e981d8eaeef909782269df1477b9d1e352c3477
Shareiar <islamshareiar@gmail.com>
2025-05-19 04:02:54 +0600

    add home page inside the pages folder and add slider component with this page

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <SlideShow />
+</template>
+
+<script>
+import SlideShow from '@/components/Slider.vue'
+export default {
+    name : 'HomePage',
+    components: {
+        SlideShow,
+    }
+}
+</script>
