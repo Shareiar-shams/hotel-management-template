38b4fc40b484bcef696a9ce19210a40d70f2cbcd
Shareiar <islamshareiar@gmail.com>
2025-05-19 04:02:14 +0600

    import router and add route with this app

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // createApp(App).mount('#app')
 
 const app = createApp(App)
+app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
-
 app.mount('#app')
